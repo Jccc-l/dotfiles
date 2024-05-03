332f35de2231eb6d6a46041bd6bf50eed062867e
Jccc <1216550215Jc@gmail.com>
2024-05-03 13:28:23 +0800

    Configure Z-Shell

---
+++ zsh/alias.zsh
@@ -0,0 +1,8 @@
+alias cnpm="npm --registry=https://registry.npmmirror.com --cache=$HOME/.npm/.cache/cnpm --disturl=https://npmmirror.com/mirrors/node --userconfig=$HOME/.cnpmrc"
+alias e="exit"
+alias ls="lsd"
+alias la="ls -a"
+alias ll="ls -llha"
+alias gia="git add"
+alias gic="git commit"
+alias gicfg="git config"
