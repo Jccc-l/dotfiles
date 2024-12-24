aa310627b8cea8cb46bb825d66146470bd9f5362
Jccc-l <1216550215Jc@gmail.com>
2024-12-24 15:55:21 +0800

    Update Neovim configuration.

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -5,13 +5,17 @@ local options = {
 	-- { "tabstop", 4 }, -- 制表符宽度为4个空格
 	-- { "shiftwidth", 4 }, -- 自动缩进宽度为4个空格
 	{ "smartindent", true }, -- 智能缩进
-	{ "wrap", false }, -- 不自动换行
+	{ "wrap", true }, -- 不自动换行
 	{ "cursorline", true }, -- 高亮当前行
 	{ "wildmode", "list:longest" }, -- 命令补全模式
 	{ "hlsearch", true },
 	{ "ignorecase", true },
 	{ "smartcase", true },
 	{ "list", true },
+	{ "scrolloff", 4 },
+	{ "sidescrolloff", 6 },
+	{ "breakindent", false },
+	-- { "breakindentopt", "shift:0" },
 }
 
 -- 迭代表并应用每个选项
